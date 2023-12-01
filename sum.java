5c00f5cdbe02f4e9a68be741e221fee6feebc37f
Harsh Pratp Tiwari <7thsilencio@gmail.com>
2023-12-01 20:18:01 +0530

    It is simple Hello World Program built in Java

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 class SumTwoNumbers {
     public static void main(String[] args) {
-        int num1 = 5;
+        int num1 = 555;
         int num2 = 10;
         int sum = num1 + num2;
         System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
